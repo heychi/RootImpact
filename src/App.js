7688ea8bb72df0e4ac942f557b345f98f8b09833
wonyoung0710 <dnjs4023@bu.ac.kr>
2025-02-13 13:34:34 +0900

    Remove unused import statement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 
@@ -6,8 +6,6 @@ import './App.css';
 import Home from './pages/Home';
 import IndexPage from './pages/Index';
 function App() {
-  console.log('hi');
-
   return (
     <Router>
       <Routes>
