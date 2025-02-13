9dede2b549b0b089a4e9b1a29ddc233790874727
heychi <heychi0728@gmail.com>
2025-02-13 14:48:39 +0900

    fix/package

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import LoginPage from './pages/LoginPage';
 import './App.css';
 import Home from './pages/Home';
 import IndexPage from './pages/Index';
+import ForwarderRecommendation from "./components/ForwarderRecommendation";
+import SmartContractInfo from "./components/SmartContractInfo";
+import SmartContractDocument from "./components/SmartContractDocument";
+import SmartContractPayment from "./components/SmartContractPayment";
+import SmartContractComplete from "./components/SmartContractComplete";
+import Dashboard from "./components/Dashboard";
+import CargoStatus from "./components/CargoStatus";
+import MonitoringDashboard from "./components/MonitoringDashboard";
+import ContractManagement from "./components/DocumentDashboard";
 function App() {
   return (
     <Router>
@@ -12,6 +21,15 @@ function App() {
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/recommendation" element={<ForwarderRecommendation />} />
+        <Route path="/contract/info" element={<SmartContractInfo />} />
+        <Route path="/contract/document" element={<SmartContractDocument />} />
+        <Route path="/contract/payment" element={<SmartContractPayment />} />
+        <Route path="/contract/complete" element={<SmartContractComplete />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/status" element={<CargoStatus />} />
+        <Route path="/dashboard/status/monitoring" element={<MonitoringDashboard />} />
+        <Route path="/dashboard/status/document" element={<ContractManagement />} />
       </Routes>
     </Router>
   );
