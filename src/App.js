9f7e38b0e723bc90af9337d14356663f5173cc85
wonyoung0710 <dnjs4023@bu.ac.kr>
2025-02-13 13:34:13 +0900

    Add redirects logic to home page when comes through index page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 
 import './App.css';
 import Home from './pages/Home';
+import IndexPage from './pages/Index';
 function App() {
   console.log('hi');
+
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
       </Routes>
