65467219c6974d67570e0de4a1b7a23448e93730
heychi <heychi0728@gmail.com>
2025-02-14 07:16:15 +0900

    feat/가라

--- src/components/Differentiators.js
+++ src/components/Differentiators.js
@@ -18,9 +18,9 @@ const Differentiators = () => {
                     <img src={diffImage1} alt="차별점 이미지 1" />
                 </div>
                 <div className="col text-col">
-                    <h3 className="detail-title">올인원(All-in-One) 디지털 포워딩 플랫폼</h3>
+                    <h3 className="detail-title">컨테이너의 남는 공간을 필요한 곳과 이어주는 효율적인 물류 솔루션</h3>
                     <p>
-                        화물 운송, 계약, 결제, 실시간 추적, 분쟁 해결까지 원스톱으로 제공하여 기존의 복잡한 물류 프로세스를 디지털화하여 간편한 운송 경험 제공합니다. 로지스메이트에서 경험해보세요.
+                        화물 운송, LCL 컨테이너 계약, 결제, 실시간 추적까지 원스톱으로 제공하여 기존의 복잡한 물류 프로세스를 디지털화하여 간편한 운송 경험 제공합니다. 로지스메이트에서 경험해보세요.
                     </p>
                 </div>
             </div>
@@ -28,9 +28,9 @@ const Differentiators = () => {
             {/* 3행: (문장, 사진) */}
             <div className="row row-3">
                 <div className="col text-col">
-                    <h3 className="detail-title">AI 기반 화주-포워더 스마트 매칭 서비스</h3>
+                    <h3 className="detail-title">컨테이너의 남는 공간 조회 및 계약</h3>
                     <p>
-                        나에게 맞는 최적의 포워더! 화물의 종류, 출발지/도착지, 운송 방식, 비용 등을 고려하여 최적의 포워더 추천하고 과거 운송 이력, 평점, 서비스 품질을 AI가 분석하여 신뢰도 높은 포워더 매칭해드립니다.
+                        포워더들이 등록한 남는 컨테이너 공간을 실시간으로 조회하고, 출발지/도착지, 운송 방식, 화물 크기 및 비용을 비교하여 가장 적합한 운송사와 신속하게 계약할 수 있습니다.
                     </p>
                 </div>
                 <div className="col image-col">
@@ -44,9 +44,9 @@ const Differentiators = () => {
                     <img src={diffImage3} alt="차별점 이미지 3" />
                 </div>
                 <div className="col text-col">
-                    <h3 className="detail-title">스마트 계약 & 안전한 결제 시스템</h3>
+                    <h3 className="detail-title">실시간 화물 관리 기능</h3>
                     <p>
-                        거래 계약까지 로지스메이트와 함께, 디지털 서명 기반 스마트 계약으로 투명하고 안전한 거래 환경을 제공합니다.
+                        운송 중인 화물의 실시간 위치 추적, 예상 도착 시간 확인, 계약 내역 및 서류 관리까지 한눈에 확인할 수 있습니다. 보다 투명하고 안전한 화물 관리를 가능하게 합니다.
                     </p>
                 </div>
             </div>
