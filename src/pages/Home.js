dcfb3f70c081e799f86131c1c4aed7c1da79f484
heychi <heychi0728@gmail.com>
2025-02-13 11:55:46 +0900

    home 수정정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import CentralSection from "../components/CentralSection";
-import Differentiators from "../components/Differentiators";
+//import Differentiators from "../components/Differentiators";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <CentralSection />
-            <Differentiators />
 
         </div>
     );
